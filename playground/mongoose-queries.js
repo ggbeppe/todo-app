ef622f271eb240074a75b44d680ed33ce6e4a696
ggbeppe <grazianigiuseppe@gmail.com>
2018-11-09 13:17:55 +0100

    Initial commit

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,32 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+const id = '5bccfbbc2616f3f576403363';
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById('5bccd11a9c964cf8745c2be7').then((user) => {
+    if (!user) {
+        return console.log('Unable to find user');
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+    console.log(e);
+});
